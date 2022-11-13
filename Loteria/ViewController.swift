6bd72bfc13e5c3958232be6e7efce4ea5a19313a
Allysson Melo <allymelox@gmail.com>
2022-11-13 08:55:10 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Allysson Melo on 13/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
